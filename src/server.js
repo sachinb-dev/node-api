d8658dab958a317ff2919b189d202696cf650089
Sachin Bhagat <sachinbhagat2696@gmail.com>
2026-06-01 19:01:29 +0530

    Created an Endpoint

--- src/server.js
+++ src/server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!, WE ARE LIVE ON VERCEL!!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Hello World! TEST ENDPOINT");
+});
+
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
